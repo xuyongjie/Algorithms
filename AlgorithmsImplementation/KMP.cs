fb028e935684d64ede3f78b22e4b124c04a339df
xuyongjie <xuyongjie1128@hotmail.com>
2016-03-23 16:58:47 +0800

    add KMP class temporarily

---
+++ AlgorithmsImplementation/KMP.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsImplementation
+{
+    class KMP
+    {
+        public string Pattern { get; private set; }
+        public int[] Next { get; private set; }
+        public KMP(string pattern)
+        {
+            this.Pattern = pattern;
+            this.Next = GetNextArray(this.Pattern);
+        }
+
+        private int[] GetNextArray(string pattern)
+        {
+            int[] next = new int[pattern.Length];
+            return next;
+        }
+
+        private int HandleNextArray(string pattern,int[] next,int i)
+        {
+
+        }
+
+        public List<int> GetAllMatchesIndex(string text)
+        {
+            return null;
+        }
+
+    }
+}
