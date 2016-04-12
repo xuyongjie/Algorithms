b8e7b4ae49ffbb9a2c8800d0e99b09827af122ea
yongjie.xu <xuyongjie1128@hotmail.com>
2016-04-12 09:24:34 +0800

    complete leetcode121 and add 309

---
+++ AlgorithmsImplementation/LeetCode/LeetCode309.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsImplementation.LeetCode
+{
+    class LeetCode309
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int max = 0;
+            int substractSum = 0;
+            int length = prices.Length;
+            for (int i = 1; i < length; i++)
+            {
+                substractSum += Math.Max(0, prices[i] - prices[i - 1]);
+                max = substractSum > max ? substractSum : max;
+            }
+            return max;
+        }
+    }
+}
