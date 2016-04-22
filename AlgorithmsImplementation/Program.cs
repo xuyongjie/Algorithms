bb133d3e37fd31a8d81a00e932032e0554a935c5
xuyongjie <xuyongjie1128@hotmail.com>
2016-04-22 17:43:24 +0800

    complete coin change

--- AlgorithmsImplementation/Program.cs
+++ AlgorithmsImplementation/Program.cs
@@ -12,43 +12,10 @@ namespace AlgorithmsImplementation
     {
         static void Main(string[] args)
         {
-            //KMP myKmp = new KMP("xuyongjie");
-            //string text = "fgshgsxuyongjiedgfagxuyongfsgdsxuyongjiexuyongjiefdsgsdf";
-            //var result = myKmp.GetAllMatchesIndex(text);
-            //foreach(var item in result)
-            //{
-            //    Console.WriteLine(item);
-            //}
-            //LeetCode5 lc = new LeetCode5();
-            //Console.WriteLine(lc.LongestPalindrome2("aadfgagawertrewq"));
-            HDOJ2602 hdoj = new HDOJ2602();
-            int T = int.Parse(Console.ReadLine());
-            int N, V;
-            while(T>0)
-            {
-                string t = Console.ReadLine();
-                string[] arr = t.Split(new char[] { ' ' });
-                N = int.Parse(arr[0]);
-                V = int.Parse(arr[1]);
-                int[] value = new int[N];
-                int[] volume = new int[N];
-                string v = Console.ReadLine();
-                string[] vArr= v.Split(new char[] { ' ' });
-                for(int i=0;i<N;i++)
-                {
-                    value[i] = int.Parse(vArr[i]);
-                }
-
-                string vol = Console.ReadLine();
-                string[] volArr = vol.Split(new char[] { ' ' });
-                for (int i = 0; i < N; i++)
-                {
-                    volume[i] = int.Parse(volArr[i]);
-                }
-
-                Console.WriteLine(hdoj.GetMaxValue(10, value, volume));
-                T--;
-            }
+            LeetCode322 lc = new LeetCode322();
+            int[] coins = { 1};
+            int amount = 3;
+            Console.WriteLine(lc.CoinChange(coins, amount));
             Console.ReadLine();
         }
     }
