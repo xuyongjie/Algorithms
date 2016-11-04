5be8dfabf6ac9900b42ec5682b4ac40387c25362
yon <xuyongjie1128@hotmail.com>
2016-11-04 16:50:25 +0800

    complete leetcode5

--- AlgorithmsImplementation/LeetCode/LeetCode55.cs
+++ AlgorithmsImplementation/LeetCode/LeetCode55.cs
@@ -19,9 +19,14 @@ namespace AlgorithmsImplementation.LeetCode
     //A = [3, 2, 1, 0, 4], return false.
     class LeetCode55
     {
+        /// <summary>
+        /// time limited error
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
         public bool CanJump(int[] nums)
         {
-            bool[] can = new bool[nums.Length];
+            bool[] can = new bool[nums.Length];//存储是否可达
             can[0] = true;
             for(int i=1;i<nums.Length;i++)
             {
@@ -34,6 +39,21 @@ namespace AlgorithmsImplementation.LeetCode
                 }
             }
             return can[nums.Length-1];
+        }
+
+        public bool CanJump2(int[] nums)
+        {
+            int[] maxReachable = new int[nums.Length];//存储当前位置下最大可达位置
+            maxReachable[0] = nums[0];
+            for(int i=1;i<nums.Length;i++)
+            {
+                maxReachable[i] = Math.Max(maxReachable[i - 1],maxReachable[i-1]>=i?i + nums[i]:maxReachable[i-1]);
+                if(i>maxReachable[i])
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
--- AlgorithmsImplementation/Program.cs
+++ AlgorithmsImplementation/Program.cs
@@ -10,8 +10,8 @@ namespace AlgorithmsImplementation
             //LeetCode388 lt = new LeetCode388();
             //Console.WriteLine(lt.LengthLongestPath("dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"));
             LeetCode55 lt = new LeetCode55();
-            Console.WriteLine(lt.CanJump(new int[] { 2, 3, 1, 1, 4 }));
-            Console.WriteLine(lt.CanJump(new int[] { 3, 2, 1, 0, 4 }));
+            Console.WriteLine(lt.CanJump2(new int[] { 3, 0, 8, 2, 0, 0, 1}));
+            //Console.WriteLine(lt.CanJump(new int[] { 3, 2, 1, 0, 4 }));
             Console.ReadLine();
         }
     }
