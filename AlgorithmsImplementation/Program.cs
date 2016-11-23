030d128602a790b53a2a9f82e1eef7cf8b75e81d
YoungJay <xuyongjie1128@hotmail.com>
2016-11-24 00:04:44 +0800

    complete leetcode145,345,453

--- AlgorithmsImplementation/Program.cs
+++ AlgorithmsImplementation/Program.cs
@@ -1,5 +1,6 @@
 using AlgorithmsImplementation.Common.Map.BFS;
 using AlgorithmsImplementation.Common.Map.DFS;
+using AlgorithmsImplementation.LeetCode;
 using System;
 
 namespace AlgorithmsImplementation
@@ -13,12 +14,15 @@ namespace AlgorithmsImplementation
             //LeetCode55 lt = new LeetCode55();
             //Console.WriteLine(lt.CanJump2(new int[] { 3, 0, 8, 2, 0, 0, 1}));
             //Console.WriteLine(lt.CanJump(new int[] { 3, 2, 1, 0, 4 }));
-            Console.WriteLine("This is BFS");
-            BFSTest test = new BFSTest();
-            test.DoBFS();
-            Console.WriteLine("\n\nThis is DFS");
-            DFSTest dfs = new DFSTest();
-            dfs.DoDFS();
+            //Console.WriteLine("This is BFS");
+            //BFSTest test = new BFSTest();
+            //test.DoBFS();
+            //Console.WriteLine("\n\nThis is DFS");
+            //DFSTest dfs = new DFSTest();
+            //dfs.DoDFS();
+            LeetCode453 lt = new LeetCode453();
+            lt.MinMoves(new int[] { 1, 2, 3 });
+            //Console.WriteLine(lt.ReverseVowels("hello"));
             Console.ReadLine();
         }
     }
